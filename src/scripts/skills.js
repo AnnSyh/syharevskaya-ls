df3bafee1c36002a7e2705ac22e5ae6d751ea441
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2021-03-19 08:27:47 +0300

    vue connection problems

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,20 @@
-console.log('this is the skills module');
+import Vue from "vue";
+
+new Vue({
+    el: "#skills-component",
+    template: '#skills-list',
+    //
+    // components:{
+    //     skills,
+    // },
+
+    // data(){
+    //     return{
+    //         skills:[],
+    //     };
+    // },
+
+    // created(){
+    //     this.skills = require("../data/skills.json");
+    // }
+})
