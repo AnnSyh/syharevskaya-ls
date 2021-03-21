8e0c25b299dddad069302076ad33cf1112c9b915
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2021-03-21 18:59:17 +0300

    add reviews slider

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./styles/main.pcss";
 import "./scripts/skills";
 import "./scripts/works";
+import "./scripts/reviews";
 
 if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
