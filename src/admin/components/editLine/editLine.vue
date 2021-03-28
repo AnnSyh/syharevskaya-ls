a18e8328efe1d926b579320875eed92ed62c5481
Anna Syharevskaya <ann.syharevskaya@gmail.com>
2021-03-28 14:56:12 +0300

    add SimpleVueValidator example

--- src/admin/components/editLine/editLine.vue
+++ src/admin/components/editLine/editLine.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="edit-line-component" :class="{'blocked' : blocked}">
+
+
+    <div class="layout-form">
+      <div class="form-group" :class="{error: validation.hasError('email')}">
+        <div class="label">* Email</div>
+        <div class="content"><input type="text" class="form-control" v-model="email"/></div>
+        <div class="message">{{ validation.firstError('email') }}</div>
+      </div>
+      <div class="form-group">
+        <div class="actions">
+          <button type="button" class="btn btn-primary" @click="submit">Submit</button>
+        </div>
+      </div>
+    </div>
+
+
+
     <div class="title" v-if="editmode === false">
       <div class="text">{{value}}</div>
       <div class="icon">
@@ -39,6 +56,13 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import SimpleVueValidation from 'simple-vue-validator';
+const Validator = SimpleVueValidation.Validator;
+
+Vue.use(SimpleVueValidation);
+
+
 export default {
   props: {
     value: {
@@ -55,8 +79,14 @@ export default {
   data() {
     return {
       editmode: this.editModeByDefault,
-      title: this.value
+      title: this.value,
+      email: ''
     };
+  },
+  validators: {
+    email: function (value) {
+      return Validator.value(value).required().email();
+    }
   },
   methods: {
     onApprove() {
@@ -65,6 +95,14 @@ export default {
       } else {
         this.$emit("approve", this.value);
       }
+    },
+    submit: function () {
+      this.$validate()
+          .then(function (success) {
+            if (success) {
+              alert('Validation succeeded!');
+            }
+          });
     }
   },
   components: {
